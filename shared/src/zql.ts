213c819cfd29b113c8f07a4271c24ead6e9b94a9
Max Koon <22125083+k2on@users.noreply.github.com>
2025-10-20 16:44:39 -0400

    debug: add print

--- shared/src/zql.ts
+++ shared/src/zql.ts
@@ -3,6 +3,7 @@ import type { AuthData } from "./auth";
 export function isLoggedIn(
   authData: AuthData | null,
 ): asserts authData is AuthData {
+  console.log("AUTHDATA", authData);
   if (!authData?.user.id) {
     throw new Error("User is not logged in");
   }
