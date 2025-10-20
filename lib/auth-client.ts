416462df76a47acda27a10615a2ab22fe1945190
Max Koon <22125083+k2on@users.noreply.github.com>
2025-10-20 10:27:57 -0400

    fix: production api url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,7 +4,7 @@ import { expoClient } from "@better-auth/expo/client";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NODE_ENV == 'production' ? 'https://money-api.koon.us' : "http://localhost:3000",
   plugins: [
     expoClient({
       scheme: "money",
