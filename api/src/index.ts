b0ebc98d42a53250abebf9a402e14d3def216c45
Max Koon <22125083+k2on@users.noreply.github.com>
2025-10-20 15:19:59 -0400

    feat: disable cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ const app = getHono();
 app.use(
   "/api/*",
   cors({
-    origin: (origin) => origin ?? "",
+    origin: "*",
     allowMethods: ["POST", "GET", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
